60c8f816c9685b33eecfae19308c0dd39eeedb0b
yongsuKim <yongsu0312.kim@gmail.com>
2023-04-02 00:59:07 +0900

    feat : [008-PRODUCT-ADD-1] 상품 추가 기능 구현
    상품 추가 scratch 생성

---
+++ order-api/src/main/java/com/order/SellerProductController.java
@@ -0,0 +1,25 @@
+package com.order;
+
+import com.order.domain.product.AddProductForm;
+import com.order.domain.product.ProductDto;
+import com.order.service.ProductService;
+import com.zerobase.domain.config.JwtAuthenticationProvider;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/seller/product")
+@RequiredArgsConstructor
+public class SellerProductController {
+
+    private final ProductService productService;
+    private final JwtAuthenticationProvider provider;
+
+    @PostMapping
+    public ResponseEntity<ProductDto> addProduct(@RequestHeader(name = "X-AUTH-TOKEN") String token,
+                                           @RequestBody AddProductForm form) {
+
+        return ResponseEntity.ok(ProductDto.from(productService.addProduct(provider.getUserVo(token).getId(), form)));
+    }
+}
